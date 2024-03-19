1608b62ba3778a5d5ac0423d9878146d254ef003
Millares <millareslazaro1@gmail.com>
2024-03-19 20:37:15 +0100

    Completado falta implementar en el codigo original

--- test_alg_Opt.py
+++ test_alg_Opt.py
@@ -1,5 +1,7 @@
 import numpy as np
-from mealpy import FloatVar, PSO
+from mealpy import FloatVar, PSO,GA,MA,SHADE,PSS,RUN,SCA,SHIO,AOA,CEM,ASO
+import time
+inicio=time.time()
 
 def objective_function(solution):
     return np.sum(solution**2)
@@ -9,13 +11,6 @@ problem_dict = {
     "obj_func": objective_function,
     "minmax": "min",
 }
-
-model = PSO.OriginalPSO(epoch=1000, pop_size=50, c1=2.05, c2=2.5, w=0.4)
-g_best = model.solve(problem_dict)
-print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
-print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
-
-#**************************Rosenbrock function PSO*************************************
 def Rosenbrock (solution,a=1.0,b=100.0):
     x=solution[0]
     y=solution[1]
@@ -26,7 +21,37 @@ problem_dict_Rosenbrock = {
     "obj_func": Rosenbrock,
     "minmax": "min",
 }
-model = PSO.OriginalPSO(epoch=1000, pop_size=50, c1=2.05, c2=2.5, w=0.4)
+
+#**************************Rosenbrock function PSO*************************************
+# model = PSO.OriginalPSO(epoch=1000, pop_size=50, c1=2.05, c2=2.5, w=0.4)
+#******************************-GEnetic Algorithm****************************************
+# model =  GA.BaseGA(epoch=10000, pop_size=50, pc=0.9, pm=0.05, crossover="arithmetic")
+#***********************************************mealpy.evolutionary_based.SHADE module*******************************************
+# model = SHADE.L_SHADE(epoch=1000, pop_size=50, miu_f = 0.5, miu_cr = 0.5)
+#***********************************************mealpy.evolutionary_based.SHADE module¶******************************************
+# model = SHADE.OriginalSHADE(epoch=1000, pop_size=50, miu_f = 0.5, miu_cr = 0.5)
+#***********************************************mealpy.math_based.RUN module¶******************************************
+# model = RUN.OriginalRUN(epoch=1000, pop_size=50)
+#************************************mealpy.math_based.SCA module¶******************************************
+# model = SCA.DevSCA(epoch=1000, pop_size=50)
+#************************************mealpy.math_based.SHIO module¶******************************************
+# model = SHIO.OriginalSHIO(epoch=1000, pop_size=50)
+#************************************mealpy.physics_based package¶******************************************
+# model = ASO.OriginalASO(epoch=1000, pop_size=100, alpha = 50, beta = 0.2)
 g_best = model.solve(problem_dict_Rosenbrock)
 print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
 print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
+
+
+
+
+
+
+
+
+
+
+
+#TODO *********************CALCULO DEL TIEMPO DE CORRIDA**************************************
+final=time.time()
+print(f'tiempo de iteracion del modelo es {final - inicio}\n')
