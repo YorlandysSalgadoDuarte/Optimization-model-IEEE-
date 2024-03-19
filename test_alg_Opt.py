3843e64829b82357d6f4be17da5c883506ca00d3
Millares <millareslazaro1@gmail.com>
2024-03-19 14:07:39 +0100

    terminado la comprobacion del modelo de optimizacion

--- test_alg_Opt.py
+++ test_alg_Opt.py
@@ -1,29 +1,32 @@
 import numpy as np
-from mealpy import FloatVar, GA
-
+from mealpy import FloatVar, PSO
 
 def objective_function(solution):
     return np.sum(solution**2)
 
 problem_dict = {
-    "bounds": FloatVar( lb=(-10.0,) * 30, ub=(10.0,) * 30, name="delta"),
+    "bounds": FloatVar( lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     "obj_func": objective_function,
     "minmax": "min",
 }
 
-model = GA.BaseGA(epoch=1000, pop_size=100, pc=0.9, pm=0.05)
+model = PSO.OriginalPSO(epoch=1000, pop_size=50, c1=2.05, c2=2.5, w=0.4)
 g_best = model.solve(problem_dict)
 print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
 print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
-model2 = GA.BaseGA(epoch=1000, pop_size=100, pc=0.9, pm=0.05, selection="tournament", k_way=0.4, crossover="multi_points")
+#**************************Rosenbrock function PSO*************************************
+def Rosenbrock (solution,a=1.0,b=100.0):
+    x=solution[0]
+    y=solution[1]
+    return (a-x)**2+b*(y-x**2)**2
 
-# model3 = GA.BaseGA(epoch=1000, pop_size=50, pc=0.9, pm=0.05, crossover="one_point", mutation="scramble")
-
-# model4 = GA.BaseGA(epoch=1000, pop_size=50, pc=0.9, pm=0.05, crossover="arithmetic", mutation_multipoints=True, mutation="swap")
-
-# model5 = GA.BaseGA(epoch=1000, pop_size=50, pc=0.9, pm=0.05, selection="roulette", crossover="multi_points")
-
-# model6 = GA.BaseGA(epoch=1000, pop_size=50, pc=0.9, pm=0.05, selection="random", mutation="inversion")
-
-# model7 = GA.BaseGA(epoch=1000, pop_size=50, pc=0.9, pm=0.05, crossover="arithmetic", mutation="flip")
+problem_dict_Rosenbrock = {
+    "bounds": FloatVar( lb=[-2.0,-1.0], ub=[2.0,3.0], name="delta"),
+    "obj_func": Rosenbrock,
+    "minmax": "min",
+}
+model = PSO.OriginalPSO(epoch=1000, pop_size=50, c1=2.05, c2=2.5, w=0.4)
+g_best = model.solve(problem_dict_Rosenbrock)
+print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
